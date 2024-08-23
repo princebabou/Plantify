ce9c851918d7ec05f9d96cdb7be650fa24947c79
princebabou <baboumanzi69@gmail.com>
2024-08-23 21:36:49 +0200

    removed some things

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./logo.png" />
+        <link rel="icon" href="./favicon.ico" />
       </head>
       <body className={poppins.className}>{children}</body>
     </html>
